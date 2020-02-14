64a286b0a50a25a396652455a0d9728e436c050e
leeyunhome <leeyunhome@daum.net>
2020-02-14 20:37:17 +0900

    add upto 4_24

---
+++ Chapter4/4_24/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_24
+{
+    class IntegerCompare : IComparer    //I Comparer를 상속받는 타입 정의
+    {
+        // IComparer 인터페이스의 Compare 메서드를 구현.
+        // 이 메서드는 Array.Sort 메서드 내에서 콜백으로 호출됨.
+        public int Compare(object x, object y)
+        {
+            int xValue = (int)x;
+            int yValue = (int)y;
+
+            if (xValue > yValue) return -1; // 내림차순 정렬이 되도록 -1을 반환
+            else if (xValue == yValue) return 0;
+
+            return 1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[] { 1, 2, 3, 4, 5 };
+
+            // IComparer를 상속받은 IntegerCompare 인스턴스 전달
+            Array.Sort(intArray, new IntegerCompare());
+            foreach (int item in intArray)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine();
+
+            string name = "leehyerim";
+            foreach (char ch in name)
+            {
+                Console.Write(ch + ", ");
+            }
+        }
+    }
+}
